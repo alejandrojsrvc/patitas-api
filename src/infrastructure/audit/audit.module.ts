34437b3d93151968b5c723011c09d6371f6f8446
Alejandro Sojo <alejandrojsrv@gmail.com>
2026-08-22 18:14:37 -0300

    fix auth module

--- src/infrastructure/audit/audit.module.ts
+++ src/infrastructure/audit/audit.module.ts
@@ -1,11 +1,12 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaModule } from '../database/prisma.module';
+import { AuthModule } from '../../modules/auth/auth.module';
 import { AdminAuditInterceptor } from './admin-audit.interceptor';
 import { AuditController } from './audit.controller';
 
 @Global()
 @Module({
-  imports: [PrismaModule],
+  imports: [AuthModule, PrismaModule],
   controllers: [AuditController],
   providers: [AdminAuditInterceptor],
   exports: [AdminAuditInterceptor],
